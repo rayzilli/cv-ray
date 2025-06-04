bcd2764a1a3f8cba5a4b105f63e245e6327c0f09
rayzilli <[200~3222523+rayzilli@users.noreply.github.com~>
2025-06-04 11:35:10 -0400

    Initialize git

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
